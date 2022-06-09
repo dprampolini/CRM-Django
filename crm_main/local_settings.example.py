254d0190f0faf68b1981a8bfe8d3222f543e9183
Davide <d.prampolini@gmail.com>
2022-06-09 13:08:01 +0200

    Create local_settings.example.py

---
+++ crm_main/local_settings.example.py
@@ -0,0 +1,20 @@
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_schema', 
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+# Email settings
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIT_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = 'SG.oDN9basdaECvH5asdasw.gXVEgtD1asqSkn-EW'
